fd219db8cb5490a7041ae3dd56b5bbffa858a777
thamara2X1 <vininduvtph@gmail.com>
2025-12-29 22:32:28 +0530

    Register playback service with TrackPlayer and enable background audio handling

--- index.js
+++ index.js
@@ -3,7 +3,13 @@
  */
 
 import { AppRegistry } from 'react-native';
+import TrackPlayer from 'react-native-track-player';
 import App from './App';
 import { name as appName } from './app.json';
+import playbackService from './src/services/playbackService';
 
+// Register the app
 AppRegistry.registerComponent(appName, () => App);
+
+// Register the playback service
+TrackPlayer.registerPlaybackService(() => playbackService);
